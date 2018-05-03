1aa93c18411e0bd85b65c4b1fb7d6e0d933c8a61
ariqnaufaal <ariq.naufal29@gmail.com>
2018-05-03 14:53:55 +0700

    Update 2

--- crud.php
+++ crud.php
@@ -6,14 +6,15 @@ if($db->connect_errno > 0){
     die("Gagal bos..!!!");
 }
 
-// if(isset($_POST['nama_barang'])){
-//     $nama = $_POST['nama_barang'];
-//     $deskripsi = $_POST['deskripsi_barang'];
-//     $harga = $_POST['harga_barang'];
+if(isset($_POST['nama_barang'])){
 
-//     $query = "INSERT INTO barang(nama, deskripi, harga) VALUES('$nama','$deskripsi', '$harga')";
-//     $db->query($query);
-// }
+    $nama = $_POST['nama_barang'];
+    $deskripsi = $_POST['deskripsi_barang'];
+    $harga = $_POST['harga_barang'];
+
+    $query = "INSERT INTO barang(nama, deskripi, harga) VALUES('$nama','$deskripsi', '$harga')";
+    $db->query($query);
+}
 
 if(isset($_GET['act']) && isset($_GET['id']))
 {
@@ -31,26 +32,13 @@ if(isset($_GET['act']) && isset($_GET['id']))
         $deskripsi = $row['deskripi'];
         $harga = $row['harga'];
 
-        if(isset($_POST['update'])){
-            $id = $row['id'];
-            $nama = $_POST['nama_barang'];
-            $deskripsi = $_POST['deskripsi_barang'];
-            $harga = $_POST['harga_barang'];
-            $update = "UPDATE barang SET nama='$nama',deskripi='$deskripsi',harga='$harga' WHERE id='$id'";
-            $result = $db->query($update);
-            if($result){
-                echo "Data berhasil diupdate <br>";
-            }
-            else{
-                echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
-            }
-        }
     }
     else if($aksi == 'delete'){
         $sql = 'DELETE FROM barang WHERE id=' . $id;
         $result = $db->query($sql);
     }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -75,8 +63,9 @@ if(isset($_GET['act']) && isset($_GET['id']))
             <h2>Form Barang</h2>
 
             <form action="crud.php" method="POST">
-            
+                
                 <div class="form-group">
+<!--                     <input type="hidden" name="id" class="form-control" id="id" value="<?php echo $data['id'] ?>"> -->
                     <label >Nama Barang</label>
                     <input type="text" name="nama_barang" value="<?php 
                     if(isset($_GET['act'])){
@@ -130,7 +119,7 @@ if(isset($_GET['act']) && isset($_GET['id']))
                 <?php
                     if(isset($_GET['act'])){
                         if($aksi == "edit"){
-                            echo '<button type="submit" name="update" class="btn btn-primary">Update</button>';
+                            echo '<button type="submit" name="updatee" class="btn btn-primary">Update</button>';
                         }
                         else{
                             echo '<button type="submit" name="simpan" class="btn btn-primary">Simpan</button>';
@@ -140,12 +129,105 @@ if(isset($_GET['act']) && isset($_GET['id']))
                     {
                         echo '<button type="submit" name="simpan" class="btn btn-primary">Simpan</button>';
                     }
+
+if(isset($nama) && isset($harga)){
+$update = $_POST['updatee'];
+if(isset($update)){
+$id = $row['id'];
+$nama = $_POST['nama_barang'];
+$deskripsi = $_POST['deskripsi_barang'];
+$harga = $_POST['harga_barang'];
+if($id !=0){
+$update = "UPDATE barang SET nama='$nama',deskripi='$deskripsi',harga='$harga' WHERE id='$id'";
+$result = $db->query($update);
+if($result){
+echo "Data berhasil diupdate <br>";
+}
+else{
+echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+}
+}    
+}
+}
                  ?>   
                 
             </form>
         </div>
     </div>
+
+
+    <br>
     <!-- ROW untuk form -->
+
+    <div class="row">
+        <div class="col-md">
+            <form action="crud.php" method="POST">
+                <div class="form-group">
+                    <label><b>Pencarian barang berdasarkan nama<b></label>
+                    <input type="text" name="cari" class="form-control" placeholder="Masukkan nama barang">
+                </div>
+                <button type="submit" name="carii" class="btn btn-primary">Cari</button>
+            </form>
+
+            <?php
+            $conn = mysqli_connect("localhost", 'root', '', 'pweb_b');
+                if(isset($_POST['cari'])){
+                    $cari = $_POST['cari'];
+                    $carii = $_POST['carii'];
+                    $sql = "SELECT * FROM barang WHERE nama like '%$cari%'";
+                    $result = mysqli_query($conn,$sql);
+                }
+                
+            ?>
+        </div>
+    </div>
+
+    <?php
+        if(isset($carii)){
+    ?>
+    <div class="row">
+        <div class="col-md">
+            <h2>Table Barang</h2>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Deskripsi</th>
+                    <th scope="col">Harga</th>
+                    <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php
+                    $no = 1;
+
+                    while($data = mysqli_fetch_array($result))
+                    {
+                    ?>
+                    <tr>
+                        <th scope="row"><?php echo $no++ ?></th>
+                        <td><?php echo $data['nama'] ?></td>  <!-- Nama Barang -->
+                        <td><?php echo $data['deskripi'] ?></td>  <!-- Deskripsi Barang -->
+                        <td><?php echo $data['harga'] ?></td>  <!-- harga Barang -->
+                        <td>
+                            <a href="crud.php?act=edit&id=<?php echo $data['id']; ?>" class="btn btn-primary">Edit</a>
+                            <a href="crud.php?act=delete&id=<?php echo $data['id']; ?>" onclick="return konfirmasi()" class="btn btn-danger">Delete</a>
+
+                        </td>
+                    </tr>
+                    <?php
+                    }
+                    $result->free();
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div> 
+    <?php
+    }
+    ?>
 
 
     <?php
@@ -172,11 +254,12 @@ if(isset($_GET['act']) && isset($_GET['id']))
 
                 <tbody>
                     <?php
+                    $no = 1;
                     while($row = $result->fetch_assoc())
                     {
                     ?>
                     <tr>
-                        <th scope="row">1</th>
+                        <th scope="row"><?php echo $no++ ?></th>
                         <td><?php echo $row['nama'] ?></td>  <!-- Nama Barang -->
                         <td><?php echo $row['deskripi'] ?></td>  <!-- Deskripsi Barang -->
                         <td><?php echo $row['harga'] ?></td>  <!-- harga Barang -->
